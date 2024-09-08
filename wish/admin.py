b8aaccd2c53dc99f845bd456353dfe6318487811
OM <orlandomedeiros.junior@gmail.com>
2024-09-08 12:26:32 -0300

    Inline WishItens to Wish.

--- wish/admin.py
+++ wish/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class InlineWishItem(admin.TabularInline):
+    model = models.WishItem
+    extra = 1
+
+
+class AdminWish(admin.ModelAdmin):
+    inlines = [
+        InlineWishItem
+    ]
+
+
+admin.site.register(models.Wish, AdminWish)
+admin.site.register(models.WishItem)
